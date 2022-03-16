3036afe98c5b1204d35529571c8882811cdacaae
Valeria Aynbinder <valeria@deeptalk-ai.com>
2022-03-16 11:22:47 +0200

    initial commit - token

--- auth_proj/auth_app/urls.py
+++ auth_proj/auth_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
 urlpatterns = [
     path("public/", views.public_view),
     path("private/", views.private_view),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- auth_proj/auth_app/views.py
+++ auth_proj/auth_app/views.py
@@ -1,15 +1,70 @@
+import http
+
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.decorators import api_view
+from rest_framework.authentication import BasicAuthentication, TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
+import base64
+
+
+from django.contrib.auth import authenticate
 
 
 @api_view(['GET'])
 def public_view(request):
     return Response({'message': 'this is a public view'})
 
+# @api_view(['GET'])
+# def private_view(request):
+#     print(request.headers)
+#     message_bytes = base64.b64decode(request.headers['Authorization'].replace("Basic ", ""))
+#     message = message_bytes.decode('ascii')
+#     print(message)
+#     username, password = message.split(":")
+#     user = authenticate(username=username, password=password)
+#     if user is not None:
+#         # A backend authenticated the credentials
+#         pass
+#     else:
+#         # No backend authenticated the credentials
+#         return Response(status=http.HTTPStatus.UNAUTHORIZED)
+#
+#     return Response({'message': 'this is a private view'})
 
+
+"""
+Option 1: manual implementation
+"""
+# @api_view(['GET'])
+# def private_view(request):
+#     print(request.headers)
+#     message_bytes = base64.b64decode(request.headers['Authorization'].replace("Basic ", ""))
+#     message = message_bytes.decode('ascii')
+#     print(message)
+#     username, password = message.split(":")
+#
+#     user = authenticate(username=username, password=password)
+#     if user is not None:
+#         # A backend authenticated the credentials
+#         pass
+#     else:
+#         # No backend authenticated the credentials
+#         return Response(status=http.HTTPStatus.UNAUTHORIZED)
+#     return Response({'message': 'this is a private view'})
+
+
+# """
+# Option 2: using decorators
+# """
+#TokenAuthentication, BasicAuthentication
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def private_view(request):
+    print(request.user)
     return Response({'message': 'this is a private view'})
